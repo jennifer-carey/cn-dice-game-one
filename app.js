1ee7b3b0ae34b44122c81d5e17c726740dc8dc6d
Jennifer <jennifacarey@gmail.com>
2021-02-05 14:26:16 +0000

    working without dice dot display

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ let score = 0;
 
 dice.style.display = "none";
 
+// add button event listener on click with condition
+rollBtn.addEventListener("click", () => {
+   if (rollBtn.textContent === "Roll") {
+      let randomNumber = Math.floor(Math.random() * 6 + 1);
+      dice.style.display = "block";
+      dice.textContent = `${randomNumber}`;
+      checkNumber(randomNumber);
+   } else if (rollBtn.textContent === "Start again") {
+      resetGame();
+   }
+})
+
 // function to reset game
 const resetGame = () => {
    if (rollBtn.textContent === "Start again") {
@@ -18,25 +30,22 @@ const resetGame = () => {
    }
 }
 
-// add button event listener on click with function
-rollBtn.addEventListener("click", () =>{
-   let randomNumber = Math.floor(Math.random() * 6 + 1);
-   console.log(randomNumber);
-   dice.style.display = "block";
-   dice.textContent = `${randomNumber}`;
-      if (randomNumber === 1) {
-         scoreInput.textContent = 0;
-         playerStatus.textContent = "You lose!";
+// function to apply condition to generated random number
+const checkNumber = (randomNumber) => {
+   if (randomNumber === 1) {
+      scoreInput.textContent = 0;
+      playerStatus.textContent = "You lose!";
+      rollBtn.textContent = "Start again";
+   } else {
+      score += randomNumber;
+      scoreInput.textContent = `${score}`;
+      if (score >= 20) {
+         playerStatus.textContent = "You win!";
          rollBtn.textContent = "Start again";
-      } else {
-         score += randomNumber;
-         scoreInput.textContent = `${score}`;
-         if (score >= 20) {
-            playerStatus.textContent = "You win!";
-            rollBtn.textContent = "Start again";
-         }
       }
-})
+   }
+}
+
 
 
 
