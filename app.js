9f5f9e580e1fc2646f20f1fc4f443bdbc2addf89
Jennifer <jennifacarey@gmail.com>
2021-02-05 09:41:18 +0000

    add comments to show process

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
+// declare variables we need
 let score = 0;
 console.log(score);
 // insert variable for roll button
 
-// button event listener on click function
+// add button event listener on click with function
 while (score < 20) {
    let randomNumber = Math.floor(Math.random() * 6 + 1);
    console.log(randomNumber);
-
+   // dice appears in DOM with randomNumber
+   // apply condition according to randomNumber produced by the dice
    if (randomNumber === 1) {
       score = 0;
-      console.log("You lose!");
+      // you lose text appears in place of Player 1 text in the DOM so replace line below
+      console.log("You lost!");
+      // roll button is removed from the DOM
+      // start again button appears above you lost text
    } else {
       score += randomNumber;
       if (score >= 20) {
+         // you win text appears in place of Player 1 text in the DOM so replace line below
          console.log("You win!");
+         // roll button is removed from the DOM
+         // start again button appears above you win text
       }
    }
 }
