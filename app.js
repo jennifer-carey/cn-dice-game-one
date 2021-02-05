45c398221f749e70b9564e32c77cc400431358de
Jennifer <jennifacarey@gmail.com>
2021-02-05 11:41:11 +0000

    update to interact with DOM

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 // declare variables we need
-let score = 0;
-console.log(score);
 const scoreInput = document.querySelector("#score");
 const dice = document.querySelector("#dice");
 const rollBtn = document.querySelector("#roll-button");
+let score = 0;
 
-//display score in the DOM
-scoreInput.textContent = `${score}`;
 
 // add button event listener on click with function
 rollBtn.addEventListener("click", () =>{
    while (score < 20) {
       let randomNumber = Math.floor(Math.random() * 6 + 1);
       console.log(randomNumber);
-      // dice appears in DOM with randomNumber
       dice.textContent = `${randomNumber}`;
       
       // apply condition according to randomNumber produced by the dice
       if (randomNumber === 1) {
+         // dice appears in DOM with randomNumber
+         // dice.textContent = `${randomNumber}`;
          score = 0;
          // you lose text appears in place of Player 1 text in the DOM so replace line below
          console.log("You lost!");
@@ -25,7 +23,10 @@ rollBtn.addEventListener("click", () =>{
          // start again button appears above you lost text
       } else {
          score += randomNumber;
+         scoreInput.textContent = `${score}`;
          if (score >= 20) {
+            // dice appears in DOM with randomNumber
+            // dice.textContent = `${randomNumber}`;
             // you win text appears in place of Player 1 text in the DOM so replace line below
             console.log("You win!");
             // roll button is removed from the DOM
