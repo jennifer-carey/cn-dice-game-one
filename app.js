3aa80167924723cabfd15ea3a7448b3a1cd3464f
Jennifer <jennifacarey@gmail.com>
2021-02-08 09:55:51 +0000

    remove repetitive/duplicate code

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ diceBox.style.display = "none";
 // event listener triggered on button click with condition
 rollBtn.addEventListener("click", () => {
    if (rollBtn.textContent === "Roll") {
-      let randomNumber = Math.floor(Math.random() * 6 + 1);
+      let randomNumber = (Math.floor(Math.random() * 6 + 1));
       checkNumber(randomNumber);
    } else if (rollBtn.textContent === "Start again") {
       resetGame();
@@ -31,22 +31,21 @@ const resetGame = () => {
 
 // function to apply condition to generated random number
 const checkNumber = (randomNumber) => {
+   diceBox.style.display = "block";
+   diceImg.src = `img/dice${randomNumber}.png`;
    if (randomNumber === 1) {
-      diceBox.style.display = "block";
-      diceImg.src = "img/dice1.png";
       scoreInput.textContent = 0;
       playerStatus.textContent = "You lose!";
       rollBtn.textContent = "Start again";
    } else {
       score += randomNumber;
       scoreInput.textContent = `${score}`;
-      diceBox.style.display = "block";
-      diceImg.src = `img/dice${randomNumber}.png`;
       if (score >= 20) {
          playerStatus.textContent = "You win!";
          rollBtn.textContent = "Start again";
       }
    }
+
 }
 
 
